54d5746c4a8960b7c903c201438c6242713db2c2
somkiat <somkiat.p@gmail.com>
2018-05-14 23:10:19 +0700

    Add new test for result activity

---
+++ app/src/androidTest/java/com/example/somkiat/calculator/ResultActivityTest.java
@@ -0,0 +1,34 @@
+package com.example.somkiat.calculator;
+
+import android.content.Intent;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+public class ResultActivityTest {
+
+    @Rule
+    public ActivityTestRule<ResultActivity> activityTestRule
+            = new ActivityTestRule<>(ResultActivity.class,
+                                    true,
+                                    false);
+
+    @Test
+    public void should_show_10_in_result_from_intent() {
+        Intent intent = new Intent();
+        intent.putExtra("result", "10");
+        activityTestRule.launchActivity(intent);
+
+        onView(withId(R.id.txv_result))
+                .check(matches(withText("Result = 10")));
+    }
+
+    
+}
